e153af44e71aec19a890ad671ea3543eec33b9f1
Michael Cooper <macoop2363@gmail.com>
2019-09-13 12:18:47 -0600

    Added test for class instantiation

--- test/offset_manager_test.rb
+++ test/offset_manager_test.rb
@@ -4,9 +4,11 @@ require './lib/offset_manager'
 class OffsetManagerTest < Minitest::Test
 
   def setup
+    @offset_manager = OffsetManager.new
   end
 
   def test_it_exists
+    assert_instance_of OffsetManager, @offset_manager
   end
 
 end
