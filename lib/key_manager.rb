cae49b8bbf486c0430d3ee75b8de7fbad1fb0c8c
Michael Cooper <macoop2363@gmail.com>
2019-09-13 11:50:22 -0600

    Added methods and their respective tests

--- lib/key_manager.rb
+++ lib/key_manager.rb
@@ -1,7 +1,28 @@
 class KeyManager
-  attr_reader
-  
-  def initialize
+  attr_reader :key,
+              :keys
+
+  def initialize(key = nil)
+    @key = key ? key : generate_key
+    @keys = generate_key_shift
   end
 
+  def generate_key
+    # generate_number
+    # if number.length < 5
+      # pad number
+    # 
+  end
+
+  def genereate_key_shift
+  end
+
+  def generate_random_number
+    rand(99999)
+  end
+
+  def pad_with_zeroes(number_string)
+    number_string.rjust(5, "0")
+  end
+    
 end
