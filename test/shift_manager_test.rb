d5d92ad5978b00ccf16eb0b8cec9ac682e7ea343
Michael Cooper <macoop2363@gmail.com>
2019-09-14 14:49:48 -0600

    Added tests for class methods

--- test/shift_manager_test.rb
+++ test/shift_manager_test.rb
@@ -6,11 +6,23 @@ require './lib/shift_manager'
 class ShiftManagerTest < Minitest::Test
   
   def setup
-    @shift_manager = ShiftManager.new
+    @key  = "01234"
+    @date = "250291"
+    @shift_manager = ShiftManager.new(@key, @date)
   end
 
   def test_it_exists
     assert_instance_of ShiftManager, @shift_manager
+  end
+
+  def test_can_generate_shifts
+    expected_hash = {
+      A: 5,
+      B: 18,
+      C: 31,
+      D: 35
+    }
+    assert_equal expected_hash, @shift_manager.generate_shifts
   end
 
 end
