bd5487fb4d651ab208b6ad325267f4140c32f8f7
Michael Cooper <macoop2363@gmail.com>
2019-09-14 13:19:12 -0600

    Added methods and their respective tests

--- lib/offset_manager.rb
+++ lib/offset_manager.rb
@@ -1,7 +1,45 @@
-class OffsetManager
-  attr_reader
+require 'date'
 
-  def initialize
+class OffsetManager
+  attr_reader :date, :offset, :offsets
+
+  def initialize(date = nil)
+    generate_new_offset(date)
+  end
+
+  def generate_new_offset(date = nil)
+    @date    = date ? date : generate_date_string
+    @offset  = generate_offset_string
+    @offsets = generate_offset_shift
+  end
+
+  def generate_offset_string
+    square_date_string(@date)[-4..-1]
+  end
+
+  def generate_offset_shift
+    {
+      "A" => @offset[0].to_i,
+      "B" => @offset[1].to_i,
+      "C" => @offset[2].to_i,
+      "D" => @offset[3].to_i
+    }
+  end
+
+  def generate_date_string
+    format_date_for_offset(current_date)
+  end
+
+  def current_date
+    Date.today
+  end
+
+  def format_date_for_offset(date)
+    date.strftime("%d%m%y")
+  end
+
+  def square_date_string(date_string)
+    (date_string.to_i ** 2).to_s
   end
 
 end
--- test/offset_manager_test.rb
+++ test/offset_manager_test.rb
@@ -4,11 +4,62 @@ require './lib/offset_manager'
 class OffsetManagerTest < Minitest::Test
 
   def setup
-    @offset_manager = OffsetManager.new
+    @offset_manager = OffsetManager.new("010120")
   end
 
   def test_it_exists
     assert_instance_of OffsetManager, @offset_manager
   end
 
+  def test_can_get_current_date
+    date = Date.today
+    assert_equal date, @offset_manager.current_date
+  end
+
+  def test_can_get_date_in_mm_dd_yy_format
+    date_string = "250291"
+    date = Date.new(1991, 02, 25)
+
+    assert_equal date_string, @offset_manager.format_date_for_offset(date)
+  end
+
+  def test_can_generate_date_string
+    @offset_manager.stubs(:current_date).returns(Date.new(1991, 02, 25))
+
+    assert_equal "250291", @offset_manager.generate_date_string
+  end
+
+  def test_can_square_date_string
+    assert_equal "62645584681", @offset_manager.square_date_string("250291")
+  end
+  
+  def test_can_generate_offset_string
+    offset_manager = OffsetManager.new("250291")
+    assert_equal "4681", offset_manager.generate_offset_string
+  end
+
+  def test_can_generate_offset_shift
+    offset_manager = OffsetManager.new("250291")
+    expected_hash = {
+      "A" => 4,
+      "B" => 6,
+      "C" => 8,
+      "D" => 1
+    }
+
+    assert_equal expected_hash, offset_manager.generate_offset_shift
+  end
+
+  def test_can_generate_new_offset
+    date    = @offset_manager.date
+    offset  = @offset_manager.offset
+    offsets = @offset_manager.offsets
+
+    @offset_manager.generate_new_offset
+
+    assert_equal true, date != @offset_manager.date
+    assert_equal true, offset != @offset_manager.offset
+    assert_equal true, offsets != @offset_manager.offsets
+  end
+    
 end
