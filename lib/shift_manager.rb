852742922160d75466ee9363ae4f58ac6451d566
Michael Cooper <macoop2363@gmail.com>
2019-09-14 14:28:15 -0600

    Added tests for functionality

--- lib/shift_manager.rb
+++ lib/shift_manager.rb
@@ -2,9 +2,16 @@ require './lib/key_manager'
 require './lib/offset_manager'
 
 class ShiftManager
-  attr_reader
+  attr_reader :shifts
 
-  def initialize
+  def initialize(key = nil, date = nil)
+    @key_manager    = KeyManager.new(key)
+    @offset_manager = OffsetManager.new(date)
+    @shifts         = generate_shifts
+  end
+
+  def generate_shifts
+    @key_manager.key_shifts.merge(@offset_manager.)
   end
 
 end
