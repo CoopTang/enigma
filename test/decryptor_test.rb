f9d386ab119c1ea1f5f01a012d6e6f058e24f94f
Michael Cooper <macoop2363@gmail.com>
2019-09-15 15:17:42 -0600

    Added decryptor test file with initial tests

---
+++ test/decryptor_test.rb
@@ -0,0 +1,47 @@
+require './test/test_helper'
+require './lib/shift_manager'
+require './lib/decryptor'
+
+class EncryptorTest < Minitest::Test
+  
+  def setup
+    @shifts = {
+      A: 5,
+      B: 18,
+      C: 31,
+      D: 35
+    }
+    @decryptor = Encryptor.new(@shifts)
+  end
+
+  def test_it_exists
+    assert_instance_of Decryptor, @decryptor
+  end
+
+  def test_can_shift_a_letter
+    assert_equal "a", @decryptor.shift_character("b", 1)
+    assert_equal "z", @decryptor.shift_character(" ", 1)
+    assert_equal "z", @decryptor.shift_character("a", 2)
+    assert_equal " ", @decryptor.shift_character("a", 1)
+    assert_equal "a", @decryptor.shift_character("a", 27)
+    assert_equal "w", @decryptor.shift_character(" ", 31)
+  end
+
+  def test_can_change_key_sets
+    new_shifts = {
+      A: 1,
+      B: 2,
+      C: 3,
+      D: 4
+    }
+    @decryptor.change_shifts(new_shifts)
+    assert_equal true, @shifts != @decryptor.shifts
+  end
+
+  def test_can_encrypt_message
+    assert_equal "hello", @decryptor.encrypt("mwptt")
+    assert_equal "hello world", @decryptor.encrypt("mwpttr wwch")
+    assert_equal "hello, user", @decryptor.encrypt("mwptt,dbxwv")
+  end
+
+end
