a28e5fb2e79725e7979dd4112a674dea02e37ff3
Michael Cooper <macoop2363@gmail.com>
2019-09-15 13:47:04 -0600

    Added tests for class method

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -8,11 +8,19 @@ class EncryptorTest < Minitest::Test
     @key  = "01234"
     @date = "250291"
     @shift_manager = ShiftManager.new(@key, @date)
-    @encryptor = Encryptor.new("Hello world", @shift_manager.shifts)
+    @encryptor     = Encryptor.new("Hello world", @shift_manager.shifts)
   end
 
   def test_it_exists
     assert_instance_of Encryptor, @encryptor
+  end
+
+  def test_can_shift_a_letter
+    assert_equal "b", @encryptor.shift_character("a", 1)
+    assert_equal " ", @encryptor.shift_character("z", 1)
+    assert_equal "a", @encryptor.shift_character("z", 2)
+    assert_equal "a", @encryptor.shift_character(" ", 1)
+    assert_equal "a", @encryptor.shift_character("a", 27)
   end
 
 end
