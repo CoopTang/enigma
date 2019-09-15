da9617a2328312b7b24ab5ee4d4f314a894b4001
Michael Cooper <macoop2363@gmail.com>
2019-09-15 13:47:21 -0600

    Added functionality to class method

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,7 +1,19 @@
+require './lib/modules/character_set'
+
 class Encryptor
-  attr_reader
+  attr_reader 
+
+  include CharacterSet
 
   def initialize(message, shifts)
+    @message  = message
+    @shifts   = shifts
+  end
+
+  def shift_character(character, number)
+    char_key     = CHAR_TO_NUM[character] + number
+    is_divisible = char_key == 27 ? 27 : 0
+    NUM_TO_CHAR[char_key % 27 + is_divisible]
   end
 
 end
