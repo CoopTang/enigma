e5bb34724bebc260cb45b0419d0513a840417331
Michael Cooper <macoop2363@gmail.com>
2019-09-15 14:56:08 -0600

    Encryptor now encrypts strings

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -5,15 +5,27 @@ class Encryptor
 
   include CharacterSet
 
-  def initialize(message, shifts)
-    @message  = message
+  def initialize(shifts)
     @shifts   = shifts
   end
 
   def shift_character(character, number)
     char_key     = CHAR_TO_NUM[character] + number
-    is_divisible = char_key == 27 ? 27 : 0
+    is_divisible = char_key % 27 == 0 ? 27 : 0
     NUM_TO_CHAR[char_key % 27 + is_divisible]
+  end
+
+  def encrypt(message)
+    shift_keys = @shifts.keys
+    encrypted = ""
+    message.chars.each_index do |index|
+      if CHAR_TO_NUM.has_key?(message[index])
+        encrypted << shift_character(message[index], @shifts[shift_keys[index % 4]])
+      else
+        encrypted << message[index]
+      end
+    end
+    encrypted
   end
 
 end
