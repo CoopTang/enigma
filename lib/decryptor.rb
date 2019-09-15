7fa1fb70d5cded2eb536b30ea0e2fa78324aac01
Michael Cooper <macoop2363@gmail.com>
2019-09-15 16:05:08 -0600

    Added class and methods for the test

---
+++ lib/decryptor.rb
@@ -0,0 +1,38 @@
+require './lib/modules/character_set'
+
+class Decryptor
+  attr_reader :shifts
+
+  include CharacterSet
+
+  def initialize(shifts)
+    @shifts         = shifts
+    @num_shift_keys = @shifts.keys.length
+  end
+
+  def decrypt(message)
+    shift_keys = @shifts.keys
+    decrypted  = ""
+    message.chars.each_index do |index|
+      if CHAR_TO_NUM.has_key?(message[index])
+        decrypted << shift_character(message[index], @shifts[shift_keys[index % @num_shift_keys]])
+      else
+        decrypted << message[index]
+      end
+    end
+    decrypted
+  end
+
+  def change_shifts(shifts)
+    @shifts         = shifts
+    @num_shift_keys = @shifts.keys.length
+  end
+
+  def shift_character(character, number)
+    num_char_set = CHAR_TO_NUM.keys.length
+    char_key     = CHAR_TO_NUM[character] - (number % num_char_set)
+    is_divisible = char_key % num_char_set == 0 ? num_char_set : 0
+    NUM_TO_CHAR[char_key % num_char_set + is_divisible]
+  end
+
+end
