0ad8f1dd730b089a47c3b319520256147ebc3c61
Michael Cooper <macoop2363@gmail.com>
2019-09-16 12:09:51 -0600

    Refactored sections of encrypt and decrypt into separate methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -12,11 +12,8 @@ class Enigma
   end
 
   def encrypt(message, key = nil, date = nil)
-    @shift_manager.generate_key(key)     unless @shift_manager.same_key?(key)
-    @shift_manager.generate_offset(date) unless @shift_manager.same_date?(date)
-
-    shifts = @shift_manager.shifts
-    @encryptor.change_shifts(shifts) unless @encryptor.same_shifts?(shifts)
+    update_shift_manager(key, date)
+    update_encryptor_shifts(@shift_manager.shifts)
     {
       encryption: @encryptor.encrypt(message),
       key: @shift_manager.key,
@@ -25,11 +22,8 @@ class Enigma
   end
 
   def decrypt(message, key, date = nil)
-    @shift_manager.generate_key(key)     unless @shift_manager.same_key?(key)
-    @shift_manager.generate_offset(date) unless @shift_manager.same_date?(date)
-
-    shifts = @shift_manager.shifts
-    @decryptor.change_shifts(shifts) unless @decryptor.same_shifts?(shifts)
+    update_shift_manager(key, date)
+    update_decryptor_shifts(@shift_manager.shifts)
     {
       decryption: @decryptor.decrypt(message),
       key: @shift_manager.key,
@@ -37,6 +31,17 @@ class Enigma
     }
   end
 
+  def update_shift_manager(key, date)
+    @shift_manager.generate_key(key)     unless @shift_manager.same_key?(key)
+    @shift_manager.generate_offset(date) unless @shift_manager.same_date?(date)
+  end
 
+  def update_encryptor_shifts(shifts)
+    @encryptor.change_shifts(shifts) unless @encryptor.same_shifts?(shifts)
+  end
+
+  def update_decryptor_shifts(shifts)
+    @decryptor.change_shifts(shifts) unless @decryptor.same_shifts?(shifts)
+  end
 
 end
