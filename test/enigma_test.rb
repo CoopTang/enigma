086bc55eb13cf8535cd95f34c5d33ece3dc4a920
Michael Cooper <macoop2363@gmail.com>
2019-09-16 06:45:14 -0600

    Added tests for class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,4 +11,22 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_can_encrypt_a_message_with_a_key_and_date
+    expected_hash = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected_hash, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_can_decrypt_a_message_with_key_and_date
+    expected_hash = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected_hash, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
 end
