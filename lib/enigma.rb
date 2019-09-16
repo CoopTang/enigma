04b94f50fa937e4f069fe01116049e382fdb83e6
Michael Cooper <macoop2363@gmail.com>
2019-09-15 20:08:10 -0600

    Added initialization

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,14 @@
+require './lib/shift_manager'
+require './lib/encryptor'
+require './lib/decryptor'
+
 class Enigma
   attr_reader
 
   def initialize
+    @shift_manager = ShiftManager.new
+    @encryptor     = Encryptor.new(@shift_manager.shifts)
+    @decryptor     = Decryptor.new(@shift_manager.shifts)
   end
 
 end
