ebd96270361d42b4c4506c858aee2476a3d39507
Michael Cooper <macoop2363@gmail.com>
2019-09-15 20:07:58 -0600

    Added initialization test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,9 +4,11 @@ require './lib/enigma'
 class EnigmaTest < Minitest::Test
 
   def setup
+    @enigma = Enigma.new
   end
 
   def test_it_exists
+    assert_instance_of Enigma, @enigma
   end
 
 end
