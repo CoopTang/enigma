5ee33d8125f3861f67f4810ed0364b820cf89105
Michael Cooper <macoop2363@gmail.com>
2019-09-16 10:13:18 -0600

    Added functionality to methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -12,11 +12,29 @@ class Enigma
   end
 
   def encrypt(message, key = nil, date = nil)
-    @shift_manager.change_shifts(@)
-    @encryptor.encrypt(message)
+    @shift_manager.generate_key(key)     unless @shift_manager.same_key?(key)
+    @shift_manager.generate_offset(date) unless @shift_manager.same_date?(date)
+
+    shifts = @shift_manager.shifts
+    @encryptor.change_shifts(shifts) unless @encryptor.same_shifts?(shifts)
+    {
+      encryption: @encryptor.encrypt(message),
+      key: @shift_manager.key,
+      date: @shift_manager.date
+    }
   end
 
   def decrypt(message, key, date = nil)
+    @shift_manager.generate_key(key)     unless @shift_manager.same_key?(key)
+    @shift_manager.generate_offset(date) unless @shift_manager.same_date?(date)
+
+    shifts = @shift_manager.shifts
+    @decryptor.change_shifts(shifts) unless @decryptor.same_shifts?(shifts)
+    {
+      decryption: @decryptor.decrypt(message),
+      key: @shift_manager.key,
+      date: @shift_manager.date
+    }
   end
 
 
